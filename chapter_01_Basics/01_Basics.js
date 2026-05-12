6d1d9e942b43d9aed72f6ca74cb447666d0fe11a
Tushar <tusharwork95@gmail.com>
2026-05-12 20:44:23 +0530

    Commit all remaining changes

--- chapter_01_Basics/01_Basics.js
+++ chapter_01_Basics/01_Basics.js
@@ -2,3 +2,11 @@ console.log("Hello, World!");
 
 let a = 10;
 console.log(a);
+
+console.log("The testing academy");
+// console.log is used to print the output in the console. 
+// It is a built-in function in JavaScript that allows you to display messages, variables, or any other information in the browser's console for debugging purposes.
+
+let x =20; // let is a keyword used to declare a variable in JavaScript. 
+// It allows you to create a block-scoped variable that can be reassigned later in the code.
+console.log(x);
